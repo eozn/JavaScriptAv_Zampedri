5b308f1d05b0710d93cd22b941b29656beed9e42
unknown <EZampedriNicolia@bpba.com.ar>
2021-10-28 09:40:43 -0300

    Ejercicio13

---
+++ PracticaObtenida/Ejercicio13/auto.js
@@ -0,0 +1,15 @@
+class Auto
+{
+  constructor( make, model, year, color )
+  {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.color = color;
+  }
+
+  setColor( color )
+  {
+    this.color = color;
+  }
+}
